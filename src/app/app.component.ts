5285246cb5bb202ed67e9cfda9567c9a6bd6be59
abedjamous <jamous.hameed94@gmail.com>
2021-08-16 21:32:53 -0500

    build the basic of reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+
+  constructor(private fb: FormBuilder){}
+
+  registrationForm = this.fb.group({
+    userName: ['', Validators.required],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
+  })
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Abed'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+
+  // loadApiData() {
+  //   this.registrationForm.setValue({
+  //     userName: 'Bruce',
+  //     password: 'test',
+  //     confirmPassword: 'test',
+  //     address: {
+  //       city: 'city',
+  //       state: 'state',
+  //       postalCode: '123456'
+  //     }
+  //   })
+  // }
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: 'Bruce',
+      password: 'test',
+      confirmPassword: 'test',
+      
+    })
+  }
 }
